e5ab86d8e348dcd769a3c21a280788386630b2d3
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-12 21:27:18 +0300

    Extract Koa application factory

---
+++ src/server/app.ts
@@ -0,0 +1,26 @@
+import * as Koa from 'koa';
+import { devMiddleware } from 'koa-webpack-middleware'
+import * as webpack from 'webpack';
+
+import config from './config';
+import * as webpackConfig from '../../src/client/webpack.config.js';
+
+export default () => {
+  const app = new Koa();
+
+  if (!config.IS_PRODUCTION) {
+    app.use(devMiddleware(webpack(webpackConfig), {
+      noInfo: false,
+      quiet: false,
+      lazy: true,
+
+      publicPath: '/assets/',
+
+      stats: {
+        colors: true
+      }
+    }));
+  }
+
+  return app;
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,31 +1,13 @@
-import * as Koa from 'koa';
 import * as koaBetterStatic from 'koa-better-static2';
 import * as koaViews from 'koa-views';
 
-import * as webpack from 'webpack';
-import { devMiddleware } from 'koa-webpack-middleware'
-
 import config from './config';
-import * as webpackConfig from '../../src/client/webpack.config.js';
 import winston from './services/winston';
-
 import router from './routes';
 
-const app = new Koa();
+import createApp from './app';
 
-if (!config.IS_PRODUCTION) {
-  app.use(devMiddleware(webpack(webpackConfig), {
-    noInfo: false,
-    quiet: false,
-    lazy: true,
-
-    publicPath: '/assets/',
-
-    stats: {
-      colors: true
-    }
-  }));
-}
+const app = createApp();
 
 app.use(koaBetterStatic('public'));
 app.use(koaViews('views'));
