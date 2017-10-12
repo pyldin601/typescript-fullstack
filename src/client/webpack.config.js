d06988837d092ff27d19d5854bb8d81eb4a2217d
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2017-10-12 21:04:29 +0300

    Add client configuration with webpack

---
+++ src/client/webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const path = require('path');
+const fs = require('fs');
+
+const entriesPath = path.resolve(__dirname, './entries');
+const entries = fs
+  .readdirSync(entriesPath)
+  .filter(filename => /tsx?$/.test(filename));
+
+module.exports = {
+  entry: entries.reduce((acc, file) => ({
+    ...acc,
+    [path.basename(file, path.extname(file))]: path.resolve(entriesPath, file),
+  }), {}),
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, '../../public/assets'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader?configFile=tsconfig.json',
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      minChunks: ({resource}) => /node_modules/.test(resource),
+    }),
+  ]
+};
